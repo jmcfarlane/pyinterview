ef5156f13b76d2c3225c32a6fea2848935dcd139
John McFarlane <john.mcfarlane@rockfloat.com>
2010-10-22 00:09:23 -0700

    Only show the "answer" button when there is a question

--- www/pyinterview.js
+++ www/pyinterview.js
@@ -1,9 +1,15 @@
 // Docuemt event handler
 $(document).ready(function () {
+  var answer_toggle = $('#answer-toggle');
   var correct_answer = $('#correct-answer');
-  correct_answer.hide();
-  
-  $('#answer-toggle').click(function () {
-    correct_answer.toggle('fast');
-  })
+
+  // Hide answer by default, hide button if no question on the page
+  if (correct_answer.length) {
+    correct_answer.hide();
+    answer_toggle.click(function () {
+      correct_answer.toggle('fast');
+    })
+  } else {
+    answer_toggle.hide();
+  }
 })

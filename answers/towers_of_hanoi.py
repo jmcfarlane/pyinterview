4d18425bae26e45aad2343b9bd9a9e1f6bded8bb
John McFarlane <john.mcfarlane@rockfloat.com>
2010-11-17 23:14:12 -0800

    Add debugging to the towers of hanoi

--- answers/towers_of_hanoi.py
+++ answers/towers_of_hanoi.py
@@ -1,7 +1,12 @@
+import os
+
 def towers(disc, src, aux, dest):
+    if os.environ.get('debug'):
+        print('call: tower(%s, %s, %s, %s)' % (disc, src, aux, dest))
+
     if disc > 0:
-        towers(disc -1, src, dest, aux)
+        towers(disc - 1, src, dest, aux)
         print 'Move disc', disc, 'from', src, 'to', dest
-        towers(disc -1, aux, src, dest)
+        towers(disc - 1, aux, src, dest)
 
 towers(3, 'Source', 'Auxillary', 'Destination')

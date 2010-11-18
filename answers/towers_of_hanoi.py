406057c876b21f6bc2d5c46c10395b4e81064bc0
John McFarlane <john.mcfarlane@rockfloat.com>
2010-11-18 00:29:11 -0800

    Even more towers debugging, and move count

--- answers/towers_of_hanoi.py
+++ answers/towers_of_hanoi.py
@@ -1,12 +1,29 @@
 import os
 
-def towers(disc, src, aux, dest):
-    if os.environ.get('debug'):
-        print('call: tower(%s, %s, %s, %s)' % (disc, src, aux, dest))
+debug = os.environ.get('debug')
+discs = int(os.environ.get('discs', 3))
+
+def towers(disc, src, aux, dest, depth, rcall=0, moves=0):
+    if debug:
+        fmt = 'call: tower(%s, %s, %s, %s) rcall=#%s, depth=%s'
+        print(fmt % (disc, src, aux, dest, rcall, depth))
 
     if disc > 0:
-        towers(disc - 1, src, dest, aux)
-        print 'Move disc', disc, 'from', src, 'to', dest
-        towers(disc - 1, aux, src, dest)
+        # Recursive call #1
+        moves += towers(disc - 1, src, dest, aux, depth + 1, 1)
 
-towers(3, 'Source', 'Auxillary', 'Destination')
+        print 'Move disc', disc, 'from', src, 'to', dest,
+        if debug:
+            print 'rcall=#%s' % rcall
+        print
+        moves += 1
+
+        # Recursive call #2
+        moves += towers(disc - 1, aux, src, dest, depth + 1, 2)
+
+    return moves
+
+moves = towers(discs, 'Source', 'Auxillary', 'Destination', 0)
+
+print
+print 'Total moves: %s' % moves
